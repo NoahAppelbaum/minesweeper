d727942659d3157a1844d03f505526468e7a9af7
Noah Appelbaum <noahappelbaum@gmail.com>
2024-01-05 16:50:34 -0600

    initial commit

---
+++ src/Game.tsx
@@ -0,0 +1,16 @@
+
+/* Game
+A game of Minesweeper, which has game state
+props: size:integer, nMines:integer
+state: board (number[][])
+
+App->Game->BlankOrRevealedSpace
+**/
+function Game({}) {
+
+    return (
+
+    )
+}
+
+export default Game;
