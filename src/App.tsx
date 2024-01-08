860d0943007b5fd705e9df1cb1193c040cc156c9
Noah Appelbaum <noahappelbaum@gmail.com>
2024-01-08 17:25:03 -0600

    Made App stateful, to apply adjustable settings to Game and Timer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import Game from './Game'
-import HeaderBar from './HeaderBar'
-import "./stylesheets/App.css"
+import { useState } from 'react';
+
+import Game from './Game';
+import HeaderBar from './HeaderBar';
+import "./stylesheets/App.css";
+
+const initialSettings = {
+  size: 8,
+  nMines: 10,
+  seconds: 60
+}
 
 function App() {
+  const [gameSettings, setGameSettings] = useState(initialSettings)
+
+  //TODO: fn(s) for changing settings
 
   return (
     <div className='App'>
-      <HeaderBar />
-      <Game size={8} nMines={10} />
+      <HeaderBar timerSeconds={gameSettings.seconds} />
+      <Game size={gameSettings.size} nMines={gameSettings.nMines} />
     </div>
   )
 }
