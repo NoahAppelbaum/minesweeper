93404668d3e7a2ca4056974ec8715b9d98f5af6d
Noah Appelbaum <noahappelbaum@gmail.com>
2024-01-08 17:24:41 -0600

    created skeleton SettingsForm component

---
+++ src/SettingsForm.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+/** SettingsForm
+ * Page to adjust game settings, which starts a new game.
+ *
+ * props:
+ * - TODO: Callbacks to change settings
+ *
+ * state:
+ * - formState -- for controlled components
+ *
+ * App -> SettingsForm
+ */
+function SettingsForm () {
+
+}
+
+export default SettingsForm;
