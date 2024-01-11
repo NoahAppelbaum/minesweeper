983ea60cef2421497f42539a7103376d68b5fe53
Noah Appelbaum <noahappelbaum@gmail.com>
2024-01-11 11:48:08 -0600

    planned additional state logic for gameState

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,14 @@ const initialSettings = {
 
 function App() {
   const [gameSettings, setGameSettings] = useState(initialSettings)
+  /*TODO: Make a state for gameState here. It can be ACTIVE, PAUSED, WON, or LOST.
+  This can be passed/drilled as needed to trigger warnings, and passed back up via callbacks */
 
   //TODO: fn(s) for changing settings
-
+  //FIXME: Timeout fn callback passing knot
   return (
     <div className='App'>
-      <HeaderBar timerSeconds={gameSettings.seconds} />
+      <HeaderBar timerSeconds={gameSettings.seconds} timeoutFn={()=>{alert("Timeout!")}}/>
       <Game size={gameSettings.size} nMines={gameSettings.nMines} />
     </div>
   )
