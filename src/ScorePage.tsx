02bc35d32c68a93e9e90f14ecc7ed786adbd98e5
Noah Appelbaum <noahappelbaum@gmail.com>
2024-03-06 17:38:56 -0600

    refactoring for score logic fix

--- src/ScorePage.tsx
+++ src/ScorePage.tsx
@@ -11,46 +11,49 @@ const ARR10 = [0,1,2,3,4,5,6,7,8,9,10];
 interface ScorePagePropsInterface {
   newScoreSeconds: number;
 }
-
+//FIXME: OK THIS WORKS NOW, BUUUUT only do the logic if score isn't 0
 function ScorePage({ newScoreSeconds }: ScorePagePropsInterface) {
-  const [scores, setScores] = useState(
-    localStorage.getItem("highScores") || new Array<number>()
-  );
-  const [newEntry, setNewEntry] = useState<number | null>(null);
+  console.log("score page got passed", newScoreSeconds);
+  // const [scores, setScores] = useState(
+  //   localStorage.getItem("highScores") || new Array<number>()
+  // );
+  // const [newEntry, setNewEntry] = useState<number | null>(null);
 
-  useEffect(() => {
-    let newScores: string | number[];
-    if (typeof scores === "string") newScores = JSON.parse(scores).scores;
-    else newScores = [...scores];
+  // useEffect(() => {
+  //   let newScores: string | number[];
+  //   if (typeof scores === "string") newScores = JSON.parse(scores).scores;
+  //   else newScores = [...scores];
 
-    if (typeof newScores === "object") {
-      for (let i = 0; i < newScores.length; i++) {
-        if (newScoreSeconds < newScores[i]) {
-          setNewEntry(i);
-          newScores.splice(i, 0, newScoreSeconds);
-        }
-      }
-    }
+  //   if (typeof newScores === "object") {
+  //     for (let i = 0; i < newScores.length; i++) {
+  //       if (newScoreSeconds < newScores[i]) {
+  //         setNewEntry(i);
+  //         newScores.splice(i, 0, newScoreSeconds);
+  //       }
+  //     }
+  //   }
 
-    setScores(newScores);
-    //TODO: account for difficulties here too, if implementing
-    localStorage.setItem("highScores", JSON.stringify({ scores: newScores }));
-  }, []);
+  //   setScores(newScores);
+  //   //TODO: account for difficulties here too, if implementing
+  //   localStorage.setItem("highScores", JSON.stringify({ scores: newScores }));
+  // }, []);
 
-  return (
-    <div className="ScorePage">
-      <h2>{getDisplayTime(newScoreSeconds)}</h2>
-      <ol className="ScorePage-scores">
-        {ARR10.map(n => {
-          return (
-            <li key={n} className={n === newEntry ? "new-entry" : ""}>
-              {scores[n] ? scores[n] : "-"}
-            </li>
-          );
-        })}
-      </ol>
-    </div>
-  );
+  // return (
+  //   <div className="ScorePage">
+  //     <h2>{getDisplayTime(newScoreSeconds)}</h2>
+  //     <ol className="ScorePage-scores">
+  //       {ARR10.map(n => {
+  //         return (
+  //           <li key={n} className={n === newEntry ? "new-entry" : ""}>
+  //             {scores[n] ? scores[n] : "-"}
+  //           </li>
+  //         );
+  //       })}
+  //     </ol>
+  //   </div>
+  // );
+
+  return <h2>{getDisplayTime(newScoreSeconds)}</h2>;
 }
 
 export default ScorePage;
