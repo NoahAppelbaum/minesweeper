788b2789544ddc34a13f4b1709e918b853e0819c
Noah Appelbaum <noahappelbaum@gmail.com>
2024-03-07 14:22:02 -0600

    working buttons to start game at different sizes

--- src/SettingsForm.tsx
+++ src/SettingsForm.tsx
@@ -1,18 +1,22 @@
-import { useState } from "react";
-
 /** SettingsForm
  * Page to adjust game settings, which starts a new game.
- *
- * props:
- * - TODO: Callbacks to change settings
- *
- * state:
- * - formState -- for controlled components
+ * Props: Callback to start game at selected size
  *
  * App -> SettingsForm
  */
-function SettingsForm () {
-
+function SettingsForm({
+  startGame,
+}: {
+  startGame: (newSettings: { size: number; nMines: number }) => void;
+}) {
+  return (
+    <div className="SettingsForm">
+      <h2>Choose Game Size</h2>
+      <button onClick={() => startGame({ size: 8, nMines: 10 })}>Small</button>
+      <button onClick={() => startGame({ size: 16, nMines: 40 })}>Medium</button>
+      <button onClick={() => startGame({ size: 22, nMines: 99 })}>Large</button>
+    </div>
+  );
 }
 
 export default SettingsForm;
